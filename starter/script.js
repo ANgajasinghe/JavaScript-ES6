9be19b7a047d3e9d29433c748130bf93c86fd745
angajasinghe@outlook.com <angajasinghe@gmail.com>
2020-05-25 20:03:02 +0530

    Strings

--- starter/script.js
+++ starter/script.js
@@ -57,3 +57,33 @@ console.log(true);
 }
 
 //console.log(a+b); //error
+
+/////////////////////////////////
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(` 
+This is ${firstName} ${lastName}. 
+He was born in ${yearOfBirth}. 
+Today, he is ${calcAge(yearOfBirth)} years old. 
+`);
+
+const n = `${firstName} ${lastName}`;
+
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes(' '));
+console.log(firstName.repeat(5));
+
