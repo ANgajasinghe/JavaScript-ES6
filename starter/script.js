d4dd2b2000db799204537d29c9cdf46e6badc2bf
ANgajasinghe <angajasinghe@gmail.com>
2020-06-07 11:35:31 +0530

    Classes and subclasses

--- starter/script.js
+++ starter/script.js
@@ -463,42 +463,113 @@ Any value (both objects and primitive values) may be used as either a key or a v
     function constuctor in ES5 ;
 */
 //ES5
+// var Person5 = function(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
+
+
+// Person5.prototype.calculateAge = function() {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+// }
+
+// var john5 = new Person5('John', 1990, 'teacher');
+
+// john5.calculateAge();
+
+// class Person6 {
+//     constructor(name , yearOfBirth , job){
+//         this.name = name;
+//         this.yearOfBirth = yearOfBirth;
+//         this.job = job; 
+//     }
+
+//     calculateAge() {
+//         var age = new Date().getFullYear() - this.yearOfBirth;
+//         console.log(age);
+//     }
+
+//     static greeting() {
+//         console.log('Hey there!');
+//     }
+// }
+
+// const john6 = new Person6('John', 1998, 'teacher');
+// john6.calculateAge();
+// Person6.greeting();
+
+/////////////////////////////////
+// Lecture: Classes and subclasses
+
+//ES5
 var Person5 = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
 
-
 Person5.prototype.calculateAge = function() {
     var age = new Date().getFullYear() - this.yearOfBirth;
     console.log(age);
 }
 
-var john5 = new Person5('John', 1990, 'teacher');
+var Athlete5 = function(name, yearOfBirth, job, olymicGames, medals) {
 
-john5.calculateAge();
+    //call a function cunstructor
+    // why this because when we creation a obj it will be a empty so we need to as that to persone prototype
+    Person5.call(this, name, yearOfBirth, job);
+    this.olymicGames = olymicGames;
+    this.medals = medals;
+}
 
+//connector 
+Athlete5.prototype = Object.create(Person5.prototype);
+
+
+//after the connector 
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+
+
+//ES6
 class Person6 {
-    constructor(name , yearOfBirth , job){
+    constructor (name, yearOfBirth, job) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
-        this.job = job; 
+        this.job = job;
     }
 
     calculateAge() {
         var age = new Date().getFullYear() - this.yearOfBirth;
         console.log(age);
     }
+}
 
-    static greeting() {
-        console.log('Hey there!');
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+    
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
     }
 }
 
-const john6 = new Person6('John', 1998, 'teacher');
-john6.calculateAge();
-Person6.greeting();
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
 
-/////////////////////////////////
-// Lecture: Classes and subclasses
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
